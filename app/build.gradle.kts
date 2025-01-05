e5b2da01c2918d17dc7bdaf2c1bec46932cfd344
Sakshi1.Chavan <Sakshi1.chavan@ril.com>
2025-01-05 21:05:38 +0530

    Added dependency for youtube access

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ android {
 }
 
 dependencies {
+    implementation("com.pierfrancescosoffritti.androidyoutubeplayer:core:12.1.0")
     implementation("androidx.media3:media3-exoplayer:1.2.1")
     implementation("androidx.media3:media3-ui:1.2.1")
     implementation("androidx.media3:media3-common:1.2.1")
